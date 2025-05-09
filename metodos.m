69c40bd59e08a5d6c7602db1300f8860137a38cd
Gustavo <gustavorbodi@gmail.com>
2025-05-09 18:01:45 -0300

    Updated

--- metodos.m
+++ metodos.m
@@ -1,8 +1,8 @@
 function result = metodos()
   n = input("");
-  disp(n);
   P = read_poly(n);
   a_b = compute_best_quota(P);
+  printf("Best quotas = [%.2f, %.2f]\n", a_b(1), a_b(2));
   x = compute_root(P, a_b(1), a_b(2));
   printf("%6f\n", x);
  endfunction
@@ -12,8 +12,7 @@ function P = read_poly(n)
   P = zeros(1, n+1);
   for k = 1:(n+1)
     expPower = n - (k-1);
-    prompt = sprintf('Insira o coeficiente para a%d (for x^%d): ', k, expPower);
-    P(k) = input(prompt);
+    P(k) = input("");
   end
 endfunction
 
@@ -30,50 +29,59 @@ endfunction
 function a_b = compute_best_quota(P);
   upper_kojima = kojima(P);
   upper_cauchy = cauchy(P);
-  candidata_minima = quota_minima(P);
-  candidata_maxima = quota_maxima(P);
-  a_b = [max(candidata_maxima, candidata_minima), min(upper_kojima, upper_cauchy)];
+  r1 = quota_minima(P);
+  r2 = quota_maxima(P);
+  a_b = [r1, min([upper_kojima, r2, upper_cauchy])];
 endfunction
 
 # Método de Cauchy
 function result = cauchy(P)
-  an = P(1);
-  lista_divisao = P(2:end) / an;
-  result = 1 + max(lista_divisao);
+  item_max = 5000;
+  tol = 1e-6;
+
+  n = length(P) - 1;
+  a1 = P(1);
+
+  x = 0;
+  for k = 1:item_max
+    soma = 0;
+    for i = 2:length(P)
+      expoente = n - i + 1;
+      soma += abs(P(i) / a1) * power(x, expoente);
+    endfor
+    x1 = power(soma, (1 / n));
+    if (abs(x1 - x) < tol)
+      break;
+    endif
+    x = x1;
+  endfor
+
+  result = x;
 endfunction
 
 # Método de Kojima
 function result = kojima(P)
-  coeficientes = P;
-  an = coeficientes(1);
-  n = length(coeficientes);
+  a1 = P(1);
+  n = length(P);
   resultados = zeros(1,n);
-  for k = 1:(n)
-    ak = coeficientes(k);
-    resultados(k) = power(abs(ak / an), (1/k));
+  for i = 1:(n-1)
+    ai1 = P(i+1);
+    resultados(i) = power(abs(ai1 / a1), (1/i));
   endfor
-  result = max(resultados);
+  sorted = fliplr(sort(resultados));
+  result = sorted(1) + sorted(2);
  endfunction
 
  # Cálculo da quota minima
- function result = quota_minima(P)
-   coeficientes = P;
-   a0 = coeficientes(end);
-   n = length(P) - 1;
-   raizes_candidatas = zeros(1,n);
-   for k = 1:(n)
-     ak = coeficientes(k);
-     raizes_candidatas(k) = power(abs( ak / a0 ), (1 / (n - k)));
-   endfor
-   minimo = min(raizes_candidatas);
-   result = minimo / (1 + minimo);
+function result = quota_minima(P)
+  den = (max(abs(P(1:end-1))) / abs(P(end))) + 1;
+  result = 1 / den;
 endfunction
 
  # Cálculo da quota maxima
 function result = quota_maxima(P)
-  coeficiente_invertidos = fliplr(P);
-  r_rev = cauchy(coeficiente_invertidos);
-  result = 1 / r_rev;
+  max_coef = max(abs(P(2:end))) / abs(P(1)) + 1;
+  result = max_coef;
 endfunction
 
 # Aplicacao secantes
